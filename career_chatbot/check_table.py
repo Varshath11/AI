90df9d1c9302c648ed865932113bcb1b0173162c
Varshath11 <varshath11@gmail.com>
2026-06-25 11:48:08 +0530

    Delete career_chatbot directory

--- career_chatbot/check_table.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("students.db")
-cursor = conn.cursor()
-
-cursor.execute("PRAGMA table_info(students)")
-print(cursor.fetchall())
-
-conn.close()
--- career_chatbot/list_models.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-API_KEY = "AQ.Ab8RN6LBu3QPY0yZTOrr0xMrZZJgEO2lQkd-PYtrHQvLnGZ83g"
-
-genai.configure(api_key=API_KEY)
-
-for model in genai.list_models():
-    print(model.name)
